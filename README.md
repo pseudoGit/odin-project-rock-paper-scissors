9d910d7709698cea49b4ccd670f92e20fc454d70
Ari <chan.ari96@gmail.com>
2022-03-30 17:41:43 -0400

    Initial commit. Finish rock paper scissors script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # odin-project-rock-paper-scissors
+Play the classic rock paper scissors game
+
+This project is to practice Javascript. Program lets the user play against a computer in the console.
---
+++ rockpaperscissors.js
@@ -0,0 +1,60 @@
+function computerPlay() {
+    let choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+        default:
+            console.log("Error.");
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == computerSelection) {
+        return "Tie!";
+    } else {
+        if (playerSelection == "rock") {
+            if (computerSelection == "paper") {
+                return "You Lose! Paper beats Rock";
+            }
+            else if (computerSelection == "scissors") {
+                return "You Win! Rock beats Scissors";
+            }
+        }
+        else if (playerSelection == "paper") {
+            if (computerSelection = "rock") {
+                return "You Win! Paper beats Rock";
+            }
+            else if (computerSelection == "scissors") {
+                return "You Lose! Scissors beats Paper";
+            }
+        }
+        else if (playerSelection == "scissors") {
+            if (computerSelection == "rock") {
+                return "You Lose! Rock beats Scissors";
+            }
+            else if (computerSelection == "paper") {
+                return "You Win! Scissors beats Paper";
+            }
+        }
+    }
+}
+
+function game() {
+    let userScore = 0, computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Enter rock, paper, or scissors: ");
+        let result = playRound(playerSelection, computerPlay());
+        if (result.includes("Win")) {
+            userScore++;
+        }
+        else {
+            computerScore++;
+        }
+    }
+    (userScore > computerScore) ? console.log(`User wins! ${userScore} to ${computerScore}`) : console.log(`Computer wins! ${userScore} to ${computerScore}`);
+}
